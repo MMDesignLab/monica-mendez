fa30556e382b5b0379616fcb1753cd369f007328
Monica Mendez <smendez22@cnm.edu>
2024-05-27 14:24:47 -0600

    Made edits and added milestone-3.html, script.js and style.css Continued to work on my layout added my expanded cards to my portfolio. Added a banner to my site.
    added footer and uploaded photos removed media query
    
    Working on layout and mobile responsiveness
    
    added content in the "about me" section and updated my section headers
    
    added bg-color to my contact me section and footer, played with colors to choose my palette
    
    updated about me into article columns and worked on my contact me form
    
    editing my html and cleaning up some css
    
    tried to create a darkmode toggle didn't work will need to remove tomorrow
    
    added a hamburger menu and made sure the back arrow worked - used javascript

--- src/main.js
+++ src/main.js
@@ -16,11 +16,25 @@ function removeActiveClasses() {
     })
 }
 
+//
+// document.getElementById('toggle').addEventListener('change',  function () {
+//     if (this.checked) {
+//         document.documentElement.classList.add('dark')
+//     } else {
+//         document.documentElement.classList.remove('dark')
+//     }
+// })
 
-document.getElementById('toggle').addEventListener('change', function () {
-    if (this.checked) {
-        document.documentElement.classList.add('dark')
-    } else {
-        document.documentElement.classList.remove('dark')
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+document.getElementById('menu').addEventListener('click', navToggle)
+
+btn.addEventListener('click', navToggle)
+    function navToggle(){
+    btn.classList.toggle('open')
+        menu.classList.toggle('flex')
+        menu.classList.toggle('hidden')
     }
-})
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['index.html', './src/**/*.{js,css}',  "./node_modules/flowbite/**/*.js"],
   theme: {
+    screens: {
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px',
+    },
     extend: {},
   },
   plugins: [
