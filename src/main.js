91769db2375c080d67dcc4cfc793b8be65a7661f
Monica Mendez <smendez22@cnm.edu>
2024-06-19 21:03:12 -0600

    made some final edits

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
 // import 'flowbite';
 
 
-
 const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 document.getElementById('menu').addEventListener('click', navToggle)
 
 btn.addEventListener('click', navToggle)
-    function navToggle(){
-    btn.classList.toggle('open')
-        menu.classList.toggle('flex')
-        menu.classList.toggle('hidden')
-    }
 
+function navToggle() {
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+}
+
+
+// function openImage(imageId) {
+//     let image = document.getElementById(imageId);
+//     image.classList.toggle('hidden'); // Show the image by removing 'hidden' class
+// }
+
+function toggleModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.classList.toggle('hidden');
+}
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'thankyou.html', './src/**/*.{js,css}',  "./node_modules/flowbite/**/*.js"],
+  content: ['index.html', 'thankyou.html', './src/**/*.{js,css}', "./node_modules/flowbite/**/*.js"],
+  darkMode: 'class',
   theme: {
     screens: {
-      sm:'480px',
-      md:'768px',
-      lg:'976px',
-      xl:'1440px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
     extend: {},
   },
   plugins: [
     require('flowbite/plugin')
-  ],
-// enable dark mode variants
-  darkMode:'media',
+  ]
 }
-
 
 
